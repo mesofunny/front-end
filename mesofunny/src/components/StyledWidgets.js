94a32224d5b37cf359a9935b0ca654d987136d02
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-07-30 15:46:15 -0700

    attempting to get a pull request

--- mesofunny/src/components/StyledWidgets.js
+++ mesofunny/src/components/StyledWidgets.js
@@ -3,9 +3,10 @@ export const jokeContainer = {
     flexWrap: 'wrap',
     justifyContent: 'center',
     color: 'white',
+    font: '1.2rem', 
 }
 export const jokeCardStyle = {
     margin: '5px',
     padding: '4px',
-    border: '1px solid black'
+    border: '1px solid black',
 }
