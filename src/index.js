4367b6305099bcbb61dbe787febf06e5570a784c
Nate Magera <nmagera07@gmail.com>
2019-07-31 17:54:11 -0400

    fixing commit issues with new branch

--- src/index.js
+++ src/index.js
@@ -2,28 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
-// import { reducer } from './store/reducers'
-// import { Provider } from "react-redux";
-// import { createStore, applyMiddleware } from 'redux';
-// import thunk from 'redux-thunk';
-// import logger from 'redux-logger'
+import { reducer } from './store/reducers'
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger'
 
 
 
-// const store = createStore(
-//     reducer, applyMiddleware(thunk, logger)
-//   );
-
-// ReactDOM.render(
-// <Provider store={store}>
-// <App />
-// </Provider>, document.getElementById('root'));
-
+const store = createStore(
+    reducer, applyMiddleware(thunk, logger)
+  );
 
 ReactDOM.render(
-    <Router>
-        <App />
-    </Router>,
-    document.getElementById('root'));
+<Provider store={store}>
+<App />
+</Provider>, document.getElementById('root'));
   
