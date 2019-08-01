96a326787d48b847f7c0f7f5f3490a0754db3b56
Ya'Kuana Davis <ydavis@bowdoin.edu>
2019-08-01 13:06:27 -0500

    WIP: Removed some files.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
-import React, { useState } from "react";
+import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import Jokes from "./components/Jokes";
-import JokeList from "./components/JokeList.js";
 import LoginRegister from './components/LoginRegister'
-import NavBar from './components/NavBar'
 import HomePage from './components/HomePage'
-import PrivateRoute from './components/PrivateRoute'
-import Test from './components/Test'
 import MenuBar from './components/MenuBar.js'
-import Profile from './components/Profile'
-import PublicFeed from './components/PublicFeed'
-import Favorites from './components/Favorites'
-import Friends from './components/Friends'
-import Inbox from './components/Inbox'
-import NewJokes from './components/NewJokes'
 import { testingBackground } from './components/StyledWidgets'
+import PublicJokes from "./components/PublicJokes";
+import Profile from "./components/Profile"; 
 
 
 
@@ -23,27 +14,15 @@ function App() {
 
   return (
       <div style={testingBackground}>
-    <>
-    <Router>
-
-    <MenuBar />
-
-      <Route exact path ='/' component={HomePage} />
-      <Route path="/" component={NavBar}/>
-      <Route path="/user" component={LoginRegister} />
-      <Route  path='/profile' component={Profile} />
-      <Route  path='/public-feed' component={PublicFeed} />
-      <Route  path='/favorites' component={Favorites} />
-      <Route  path='/friends' component={Friends} />
-      <Route  path='/inbox' component={Inbox} />
-      <Route  path='/new-jokes' component={NewJokes} />
-
-
-
-      
-      <PrivateRoute path="/test" component={Test} />
-      </Router>
-    </>
+        <>
+        <Router>
+          <MenuBar />
+            <Route exact path ='/' component={HomePage} />
+            <Route path="/user" component={LoginRegister} />
+            <Route path='/profile' component={Profile} />
+            <Route path='/public-feed' component={PublicJokes} />
+          </Router>
+        </>
       </div>
   );
 }
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -5,47 +5,68 @@ import menubaremoji from '../ImgFiles/menubaremoji.PNG';
 import { menuBarEmoji } from './StyledWidgets';
 
  const MenuBar = () => {
-    return(
-        <div>
-        <Menu attached='top'>
-            <Dropdown item icon='bars' simple>
-                    <Dropdown.Menu>
-                        <div>
-                        <Link to='/' >
-                            <Dropdown.Item>Home</Dropdown.Item>
-                        </Link>
-                        </div>
-                        <div>
-                        <Link to='/profile' >
-                            <Dropdown.Item>Profile</Dropdown.Item>
-                        </Link>
-                        </div>
-                        <div>
-                        <Link to ='/public-feed'>
-                            <Dropdown.Item>Public Feed</Dropdown.Item>
-                        </Link>
-                        </div>
-                        <Link to ='favorites'>
-                            <Dropdown.Item>Favorites</Dropdown.Item>
-                        </Link>
-                        <Link to ='friends'>
-                            <Dropdown.Item>Friends</Dropdown.Item>
-                        <Dropdown.Item />
-                        </Link>
-                        <Link to ='inbox'>
-                            <Dropdown.Item>Inbox</Dropdown.Item>
-                        </Link>
-                        <Link to ='new-jokes'>
-                            <Dropdown.Item>New Jokes</Dropdown.Item>
-                        </Link>
-                    </Dropdown.Menu>
-            </Dropdown>
-            <Menu.Menu position='left'>
-                <img src={menubaremoji} alt='menuBarEmoji' style={menuBarEmoji}/>
-                <h1>MeSoFunny</h1>
-            </Menu.Menu>
-        </Menu>
-          </div>
+    const token = localStorage.getItem('token')
+    
+    if (token) {
+        return (
+            <div>
+                <Menu attached='top'>
+                    <Dropdown item icon='bars' simple>
+                        <Dropdown.Menu>
+                            <div>
+                            <Link to='/' >
+                                <Dropdown.Item>Home</Dropdown.Item>
+                            </Link>
+                            </div>
+                            <div>
+                            <Link to='/contact' >
+                                <Dropdown.Item>Contact Us</Dropdown.Item>
+                            </Link>
+                            </div>
+                            <div>
+                            <Link to ='/profile'>
+                                <Dropdown.Item>Profile</Dropdown.Item>
+                            </Link>
+                            </div>
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+                    <Menu.Menu position='left'>
+                        <img src={menubaremoji} alt='menuBarEmoji' style={menuBarEmoji}/>
+                        <h1>MeSoFunny</h1>
+                    </Menu.Menu>
+            
+                </Menu>
+            </div>
         )
+    } else {
+
+        return (
+            <div>
+                <Menu attached='top'>
+                    <Dropdown item icon='bars' simple>
+                        <Dropdown.Menu>
+                            <div>
+                            <Link to='/' >
+                                <Dropdown.Item>Home</Dropdown.Item>
+                            </Link>
+                            </div>
+                            <div>
+                            <Link to ='/public-feed'>
+                                <Dropdown.Item>Public Feed</Dropdown.Item>
+                            </Link>
+                            </div>
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+                    <Menu.Menu position='left'>
+                        <img src={menubaremoji} alt='menuBarEmoji' style={menuBarEmoji}/>
+                        <h1>MeSoFunny</h1>
+                    </Menu.Menu>
+
+                </Menu>
+            </div>
+        )
+    }
 }
 export default MenuBar;
